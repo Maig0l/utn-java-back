bca60667e3a8aacf4c511b18f3b16cb295412e0c
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-10-25 22:55:13 -0300

    chore: Init Spring Boot project

---
+++ src/test/java/gg/wellplayed/backend/WellplayedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.wellplayed.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellplayedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
