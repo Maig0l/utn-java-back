f5d9aa479e9fb10713f17a7ef6b7c75ccfc74551
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-11-26 15:30:05 -0300

    fix: CORS Origins (set to *)

--- src/main/java/gg/wellplayed/backend/config/SecurityConfig.java
+++ src/main/java/gg/wellplayed/backend/config/SecurityConfig.java
@@ -5,13 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
 				.csrf(csrf ->
 					csrf
@@ -27,13 +32,21 @@ public class SecurityConfig {
 						.anyRequest().permitAll()
 				)
 				.formLogin(withDefaults())
-				.headers().disable()
+				.headers(headers -> headers.disable())
 				.build();
 		
 	}
 	
-	
-	 
+	@Bean
+	UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		//TODO: Cambiar "*". Los orígenes admitidos deberían ser http://localhost:4200 (sólo durante desarrollo) o http://wellplayed.gg:80
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
 
-	
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
 }
