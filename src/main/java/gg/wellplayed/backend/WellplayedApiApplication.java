cb8e96a02dc6e49283da446a6fed7209f7f805f9
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-11-27 02:42:25 -0300

    feat: Add check for Env variables at startup

--- src/main/java/gg/wellplayed/backend/WellplayedApiApplication.java
+++ src/main/java/gg/wellplayed/backend/WellplayedApiApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yaml.snakeyaml.error.MissingEnvironmentVariableException;
 
 @SpringBootApplication
 @RestController
 public class WellplayedApiApplication {
 
 	public static void main(String[] args) {
+		checkEnvironment();
 		SpringApplication.run(WellplayedApiApplication.class, args);
 	}
 	
@@ -18,4 +20,17 @@ public class WellplayedApiApplication {
 		return "Welcome.";
 	}
 
+	private static void checkEnvironment() throws MissingEnvironmentVariableException {
+		String[] vars = {
+				"DB_CONN_STRING",
+				"DB_PASS",
+				"DB_USER",
+				"API_SECRET"};
+
+		for (String var : vars) {
+			if (System.getenv(var) == null) {
+				throw new MissingEnvironmentVariableException("Falta la variable de entorno " + var);
+			}
+		}
+	}
 }
